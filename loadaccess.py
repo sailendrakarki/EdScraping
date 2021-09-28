0ce704a0970028d573b2b25cf1ca400ccb1df8ba
Sahil Karki <skarki@unr.edu>
2021-09-28 16:00:27 -0700

    add regrx

--- loadaccess.py
+++ loadaccess.py
@@ -1,20 +1,25 @@
 import pyodbc
+import re
 
 
 pyodbc.lowercase = False
-accessPath = r'C:\\Users\skarki\\Downloads\\IPEDS2000405.accdb'
-
+accessPath = r'C:\\Users\skarki\\Downloads\\IPEDS200405.accdb'
+excludelist = ['MSysAccessStorage','MSysACEs','MSysComplexColumns','MSysNameMap',
+               'MSysNavPaneGroupCategories','MSysNavPaneGroups','MSysNavPaneGroupToObjects',
+               'MSysNavPaneObjectIDs','MSysObjects','MSysQueries','MSysRelationships',
+               'MSysResources']
 conn = pyodbc.connect(
     r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};" +
     r"Dbq=C:\\Users\skarki\\Downloads\\IPEDS200405.accdb;")
 cursor = conn.cursor()
 
 try:
-    cursor.execute('''GRANT SELECT ON MSysObjects TO Admin;''')
+    for row in cursor.tables():
+        if row.table_name not in excludelist:  
+            print(row.table_name + "  ::  " + re.sub(r"\d{4}|\d{2}","",row.table_name)+"::")
+            
    
 except pyodbc.DatabaseError as error:
     print(error)
-else:
-    result = cursor.fetchone()
-    print(result)
+
 
